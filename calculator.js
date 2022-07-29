123bb8ada7fa5b6f9d3882620b488a3e14bd570c
Oguzcan Aygun <aygunoguz51@gmail.com>
2022-07-29 17:27:39 +0300

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -4,18 +4,6 @@ var bodyParser=require('body-parser');
 
 app.use(bodyParser.urlencoded({extended:true}));
 
-app.get('/',function(req,res){
-  res.sendFile(__dirname+"/index.html");
-})
-
-app.post("/",function(req,res){
-  var num1=Number(req.body.n1);
-  var num2=Number(req.body.n2);
-
-  var result=num1+num2;
-
-  res.send("The result is: "+result);
-})
 
 app.get("/bmicalculator",function(req,res){
   res.sendFile(__dirname+"/bmiCalculator.html");
